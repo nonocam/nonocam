7f62a363c11722841fec7645a32610312e74cf65
Valentin Sawadski <valentin@sawadski.eu>
2022-10-14 09:22:08 +0200

    refactor: Moves pages into src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Component, createRef, RefObject, useRef, useState } from 'react'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import Script from 'next/script'
 
 declare let cocoSsd: any;
