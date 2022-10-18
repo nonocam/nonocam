0fc5c9ffe326a27d6ec7dcb6d21f58cbed67ce56
Valentin Sawadski <valentin@sawadski.eu>
2022-10-19 00:05:10 +0200

    Docs

---
+++ docs/_next/static/yG7nbWYVXWPCj25NJtXNC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/967-931b8c31f546e830.js","static/css/78ef93c28a9a169c.css","static/chunks/pages/index-053cf4dae20d359e.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
